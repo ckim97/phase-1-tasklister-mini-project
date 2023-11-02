51b0a5567bfa796c8b5988eec2eaee5b46004d68
Christopher <chrisjwkim97@gmail.com>
2023-11-02 09:40:56 -0400

    lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("click", (e) => {
+    e.preventDefault();
+    const task = document.getElementById("new-task-description").value.trim();
+
+    if(task) {
+      createTask(task);
+    }
+    form.reset();
+  })
 });
+
+function createTask(todo) {
+  let list = document.getElementById("tasks");
+  let li = document.createElement("li");
+  li.innerText = `${todo} `;
+  list.append(li);
+  let button = document.createElement("button");
+  button.addEventListener("click", deleteButton);
+  button.innerText = "[x]";
+  li.append(button);
+}
+
+function deleteButton(e) {
+  e.target.parentNode.remove();
+}
